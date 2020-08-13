0e0d2cb26d71b3ec42c4857f36b84381c0a9dc12
Calvin Reed <calvintreed1@gmail.com>
2020-08-12 18:55:04 -0600

    Still working on contact form.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const Recaptcha = require('express-recaptcha').RecaptchaV2
-const {check, validationResult}= require("express-validator")
+const {check, validationResult} = require("express-validator")
 const mailgun = require('mailgun-js')
 
 //application variable to allow express to run in this file
@@ -10,15 +10,14 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
-
 
 const requestValidation = [
   check("email", "A valid email is required.").isEmail().normalizeEmail(),
   check("name", "a Valid name is required.").not().isEmpty().trim().escape(),
-  check("message", "A message is required to send an email.").not().isEmpty().trim().escape().isLength({max:2000})
+  check("message", "A message is required to send an email.").not().isEmpty().trim().escape().isLength({max: 2000})
 ]
 
 const indexRoute = express.Router()
@@ -27,19 +26,18 @@ const indexRouteMiddleware = (request, response, nextFunction) => {
   return response.json("express server is live")
 }
 
-
 const handleEmailPost = (request, response, nextFunction) => {
-  response.append('Content-Type','text/html')
+  response.append('Content-Type', 'text/html')
   if (request.recaptcha.error) {
     return response.send(`<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong>There was an error with Recaptcha please try again</div>`)
   }
   const errors = validationResult(request)
-    console.log ("madeitpasterrorcheck1")
+  console.log("madeitpasterrorcheck1")
   if (!errors.isEmpty()) {
     const currentError = errors.array()[0]
     return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong> ${currentError.msg}</div>`))
   }
-      console.log ("madeitpasterrorcheck2")
+  console.log("madeitpasterrorcheck2")
   const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN})
   const {email, name, message} = request.body
 
@@ -49,7 +47,7 @@ const handleEmailPost = (request, response, nextFunction) => {
     subject: `${name} - ${email}`,
     text: message
   }
-    console.log ("madeitpasterrorcheck3")
+  console.log("madeitpasterrorcheck3")
   mg.messages().send(mailgunData, (error) => {
     if (error) {
       return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong> Unable to send email error with email sender</div>`))
@@ -58,8 +56,6 @@ const handleEmailPost = (request, response, nextFunction) => {
   })
 
 }
-
-
 
 indexRoute.route('/')
   .get(indexRouteMiddleware)
