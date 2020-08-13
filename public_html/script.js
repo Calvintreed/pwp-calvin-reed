55a862c208808cb0b9bd851454929cbd26ba62cf
Calvin Reed <calvintreed1@gmail.com>
2020-08-13 17:09:24 -0600

    Working on styling changes and getting the contact form to function.

--- public_html/script.js
+++ public_html/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   initModal()
 }
 const initModal = () => {
-  let modals = document.querySelectorAll('.pop')
+  let modals = document.querySelectorAll('.wow')
   modals.forEach((element) => {
     element.addEventListener("click", (e) => {
       document.querySelector(`.image-preview`).src = element.src
