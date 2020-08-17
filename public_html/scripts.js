63b593618e70b27068eb174c9888c7f3f0c2b613
Calvin Reed <calvintreed1@gmail.com>
2020-08-17 16:35:36 -0600

    Got remaining 3 modals working, as well as some stylistic changes in regards to the pointer.

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,3 +1,15 @@
 $('#trigger-personal').click('shown.bs.modal', function () {
   $('#personalPortfolio').modal('show')
 })
+
+$('#trigger-concert').click('shown.bs.modal', function () {
+  $('#concertCrawl').modal('show')
+})
+
+$('#trigger-show').click('shown.bs.modal', function () {
+  $('#photoShow').modal('show')
+})
+
+$('#trigger-current').click('shown.bs.modal', function () {
+  $('#photographyWork').modal('show')
+})
