01bd1927c71526d4eb22adbdaf04feac308fec06
Calvin Reed <calvintreed1@gmail.com>
2020-08-18 13:43:58 -0600

    Added some final comments.

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,19 +1,20 @@
+//Triggering Personal Portfolio Modal
 $('#trigger-personal').click('shown.bs.modal', function () {
   $('#personalPortfolio').modal('show')
 })
-
+//Triggering Concert Crawl Modal
 $('#trigger-concert').click('shown.bs.modal', function () {
   $('#concertCrawl').modal('show')
 })
-
+//Triggering Photography Show Modal
 $('#trigger-show').click('shown.bs.modal', function () {
   $('#photoShow').modal('show')
 })
-
+//Triggering Current Photography Modal
 $('#trigger-current').click('shown.bs.modal', function () {
   $('#photographyWork').modal('show')
 })
-
+//Carousel interval in Current Photography Modal
 $('.carousel').carousel({
   interval: 2000
 })
